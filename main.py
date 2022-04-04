5d280faceb91ff8ff676b0405143f52d9fe8e8c4
RybaPila-IT <r.radziukiewicz2@gmail.com>
2022-04-04 08:15:01 +0200

    Finished lexical analyzer implementation

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 # For now, this is a dead script.
 if __name__ == '__main__':
-    print('This is where my {} will be!'.format('awesome program'))
+    print('This is where my {} will be!'.format('finished lexical analyzer'))
