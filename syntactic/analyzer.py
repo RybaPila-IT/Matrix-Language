78711c5b2739ffe73e8eba158953607a42f20de0
RybaPila-IT <r.radziukiewicz2@gmail.com>
2022-04-17 13:51:42 +0200

    Introduce _then_next logic
    What? Changed functions to fetch new token
    after successful token type checking or
    value obtaining.
    
    Why? I spotted that the pattern:
    'Grab value and fetch next token' or
    'Check token type and if matches fetch next one'
    is very common and therefore introduced this
    helper functions.
    They have definitely shortened the overall
    source code length :)

--- syntactic/analyzer.py
+++ syntactic/analyzer.py
@@ -21,41 +21,34 @@ class SyntacticAnalyzer:
     def __try_parse_function_definition(self):
         if not self.__is_token(TokenType.IDENTIFIER):
             return None
-        identifier = self.__current_token_value()
-        if not self.__is_next_token(TokenType.OPEN_ROUND_BRACKET):
+        identifier = self.__current_token_value_then_next()
+        if not self.__is_token_then_next(TokenType.OPEN_ROUND_BRACKET):
             raise MissingBracketException(TokenType.OPEN_ROUND_BRACKET, self.__current_token())
-        # Consume token with opening square bracket
-        # in order to maintain invariant.
-        self.__next_token()
         parameters = self.__try_parse_parameters()
-        if not self.__is_token(TokenType.CLOSE_ROUND_BRACKET):
+        if not self.__is_token_then_next(TokenType.CLOSE_ROUND_BRACKET):
             raise MissingBracketException(TokenType.CLOSE_ROUND_BRACKET, self.__current_token())
-        # Consume token with closing square bracket
-        # in order to maintain invariant.
-        self.__next_token()
-        statement_block = self.__try_parse_statement_block()
-        if statement_block is None:
+        if (statement_block := self.__try_parse_statement_block()) is None:
             raise MissingBracketException(TokenType.OPEN_CURLY_BRACKET, self.__current_token())
+
         return FunctionDefinition(identifier, parameters, statement_block)
 
     def __try_parse_parameters(self):
         if not self.__is_token(TokenType.IDENTIFIER):
             return []
-        identifiers = [Identifier(self.__current_token_value())]
-        while self.__is_next_token(TokenType.COMMA):
+        identifiers = [Identifier(self.__current_token_value_then_next())]
+        while self.__is_token_then_next(TokenType.COMMA):
             # Since the ',' sign appeared, the identifier must follow.
-            if not self.__is_next_token(TokenType.IDENTIFIER):
+            if not self.__is_token(TokenType.IDENTIFIER):
                 raise MissingIdentifierException(self.__current_token())
-            identifiers.append(Identifier(self.__current_token_value()))
+            identifiers.append(Identifier(self.__current_token_value_then_next()))
 
         return identifiers
 
     def __try_parse_statement_block(self):
-        if not self.__is_token(TokenType.OPEN_CURLY_BRACKET):
+        if not self.__is_token_then_next(TokenType.OPEN_CURLY_BRACKET):
             return None
-        self.__next_token()
         statements = []
-        while not self.__is_token(TokenType.CLOSE_CURLY_BRACKET):
+        while not self.__is_token_then_next(TokenType.CLOSE_CURLY_BRACKET):
             for try_parse in [self.__try_parse_if_statement,
                               self.__try_parse_until_statement,
                               self.__try_parse_return_statement,
@@ -66,35 +59,28 @@ class SyntacticAnalyzer:
                     # but in reality is not.
                     raise UnexpectedTokenException(self.__current_token())
                 statements.append(result)
-        # Consume closing '}' bracket token
-        # in order to maintain invariant.
-        self.__next_token()
-
+        # The '}' is already consumed by the '__is_token_then_next' method.
         return StatementBlock(statements)
 
     def __try_parse_if_statement(self):
-        if not self.__is_token(TokenType.IF):
+        if not self.__is_token_then_next(TokenType.IF):
             return None
-        self.__next_token()
         condition = self.__try_parse_mandatory_parenthesised_or_condition()
         if (statement_block := self.__try_parse_statement_block()) is None:
             raise UnexpectedTokenException(self.__current_token())
-        if not self.__is_token(TokenType.ELSE):
+        if not self.__is_token_then_next(TokenType.ELSE):
             return IfStatement(condition, statement_block)
-        # Consuming 'else' token.
-        self.__next_token()
         for try_parse in [self.__try_parse_statement_block,
                           self.__try_parse_if_statement]:
             if (else_statement := try_parse()) is not None:
                 return IfStatement(condition, statement_block, else_statement)
 
-        # After the 'else' statement the other statement must follow.
+        # After the 'else' constant the statement must follow.
         raise UnexpectedTokenException(self.__current_token())
 
     def __try_parse_until_statement(self):
-        if not self.__is_token(TokenType.UNTIL):
+        if not self.__is_token_then_next(TokenType.UNTIL):
             return None
-        self.__next_token()
         condition = self.__try_parse_mandatory_parenthesised_or_condition()
         if (statement_block := self.__try_parse_statement_block()) is None:
             raise UnexpectedTokenException(self.__current_token())
@@ -102,21 +88,18 @@ class SyntacticAnalyzer:
         return UntilStatement(condition, statement_block)
 
     def __try_parse_mandatory_parenthesised_or_condition(self):
-        if not self.__is_token(TokenType.OPEN_ROUND_BRACKET):
+        if not self.__is_token_then_next(TokenType.OPEN_ROUND_BRACKET):
+            # The or condition is mandatory, so the '(' token must be present.
             raise MissingBracketException(TokenType.OPEN_ROUND_BRACKET, self.__current_token())
-        # Consuming the '(' token.
-        self.__next_token()
         if (condition := self.__try_parse_or_condition()) is None:
             raise UnexpectedTokenException(self.__current_token())
-        if self.__is_token(TokenType.CLOSE_ROUND_BRACKET):
+        if not self.__is_token_then_next(TokenType.CLOSE_ROUND_BRACKET):
             raise MissingBracketException(TokenType.CLOSE_ROUND_BRACKET, self.__current_token())
-        # Consuming the ')' token.
-        self.__next_token()
 
         return condition
 
     def __try_parse_return_statement(self):
-        if not self.__is_token(TokenType.RETURN):
+        if not self.__is_token_then_next(TokenType.RETURN):
             return None
 
         return ReturnStatement(self.__try_parse_additive_expression())
@@ -130,10 +113,9 @@ class SyntacticAnalyzer:
         mul_expressions = [mul_expression]
         operators = []
         while self.__is_token(TokenType.ADD) or self.__is_token(TokenType.MINUS):
-            operators.append(self.__current_token_value())
-            self.__next_token()
+            operators.append(self.__current_token_value_then_next())
             if (mul_expression := self.__try_parse_multiplicative_expression()) is None:
-                # After the operand, there must be a multiplicative expression.
+                # After '+' or '-' operand there must be a multiplicative expression.
                 raise UnexpectedTokenException(self.__current_token())
             mul_expressions.append(mul_expression)
 
@@ -145,10 +127,9 @@ class SyntacticAnalyzer:
         atomic_expressions = [atomic_expression]
         operators = []
         while self.__is_token(TokenType.MULTIPLY) or self.__is_token(TokenType.DIVIDE):
-            operators.append(self.__current_token_value())
-            self.__next_token()
+            operators.append(self.__current_token_value_then_next())
             if (atomic_expression := self.__try_parse_atomic_expression()) is None:
-                # After the operand, there mus be an atomic expression.
+                # After the '*' or '/' operand there mus be an atomic expression.
                 raise UnexpectedTokenException(self.__current_token())
             atomic_expressions.append(atomic_expression)
 
@@ -156,31 +137,27 @@ class SyntacticAnalyzer:
 
     def __try_parse_atomic_expression(self):
         negated = False
-        if self.__is_token(TokenType.MINUS):
+        if self.__is_token_then_next(TokenType.MINUS):
             negated = True
-            self.__next_token()
 
         for try_parse in [self.__try_parse_identifier_or_function_call,
                           self.__try_parse_parenthesised_or_condition,
                           self.__try_parse_literal]:
             if (atomic_term := try_parse()) is not None:
                 return AtomicExpression(negated, atomic_term)
-
+        # If nwe were unable to parse atomic expression, it means an error at this point.
         raise UnexpectedTokenException(self.__current_token())
 
     def __try_parse_identifier_or_function_call(self):
         if not self.__is_token(TokenType.IDENTIFIER):
             return None
-        identifier = self.__current_token_value()
+        identifier = self.__current_token_value_then_next()
         # Consume current identifier value and check if we have the '(' token.
-        if not self.__is_next_token(TokenType.OPEN_ROUND_BRACKET):
+        if not self.__is_token_then_next(TokenType.OPEN_ROUND_BRACKET):
             return Identifier(identifier)
-        # Consume the '(' token.
-        self.__next_token()
         args = self.__try_parse_arguments()
-        if not self.__is_token(TokenType.CLOSE_ROUND_BRACKET):
+        if not self.__is_token_then_next(TokenType.CLOSE_ROUND_BRACKET):
             raise MissingBracketException(TokenType.CLOSE_ROUND_BRACKET, self.__current_token())
-        self.__next_token()
 
         return FunctionCall(identifier, args)
 
@@ -188,9 +165,7 @@ class SyntacticAnalyzer:
         if (expression := self.__try_parse_additive_expression()) is None:
             return []
         arguments = [expression]
-        while self.__is_token(TokenType.COMMA):
-            # Consuming the ',' token.
-            self.__next_token()
+        while self.__is_token_then_next(TokenType.COMMA):
             if (expression := self.__try_parse_additive_expression()) is None:
                 # After the comma, there must be an expression.
                 raise UnexpectedTokenException(self.__current_token())
@@ -199,16 +174,12 @@ class SyntacticAnalyzer:
         return arguments
 
     def __try_parse_parenthesised_or_condition(self):
-        if not self.__is_token(TokenType.OPEN_ROUND_BRACKET):
+        if not self.__is_token_then_next(TokenType.OPEN_ROUND_BRACKET):
             return None
-        self.__next_token()
-        or_condition = self.__try_parse_or_condition()
-        if or_condition is None:
+        if (or_condition := self.__try_parse_or_condition()) is None:
             raise UnexpectedTokenException(self.__current_token())
-        if not self.__is_token(TokenType.CLOSE_ROUND_BRACKET):
+        if not self.__is_token_then_next(TokenType.CLOSE_ROUND_BRACKET):
             raise MissingBracketException(TokenType.CLOSE_ROUND_BRACKET, self.__current_token())
-        # Consume ')' in order to maintain invariant.
-        self.__next_token()
 
         return or_condition
 
@@ -216,8 +187,7 @@ class SyntacticAnalyzer:
         if (and_condition := self.__try_parse_and_condition()) is None:
             return None
         and_conditions = [and_condition]
-        while self.__is_token(TokenType.OR):
-            self.__next_token()
+        while self.__is_token_then_next(TokenType.OR):
             if (and_condition := self.__try_parse_and_condition()) is None:
                 # After the or operand, there mus be an and condition.
                 raise UnexpectedTokenException(self.__current_token())
@@ -229,8 +199,7 @@ class SyntacticAnalyzer:
         if (rel_condition := self.__try_parse_relation_condition()) is None:
             return None
         rel_conditions = [rel_condition]
-        while self.__is_token(TokenType.AND):
-            self.__next_token()
+        while self.__is_token_then_next(TokenType.AND):
             if (rel_condition := self.__try_parse_relation_condition()) is None:
                 # After the and operand, there mus be an relation condition.
                 raise UnexpectedTokenException(self.__current_token())
@@ -240,9 +209,8 @@ class SyntacticAnalyzer:
 
     def __try_parse_relation_condition(self):
         negated = False
-        if self.__is_token(TokenType.NOT):
+        if self.__is_token_then_next(TokenType.NOT):
             negated = True
-            self.__next_token()
         if (left_expression := self.__try_parse_additive_expression()) is None:
             raise UnexpectedTokenException(self.__current_token())
         possible_token_types = [
@@ -255,8 +223,7 @@ class SyntacticAnalyzer:
         ]
         for token_type in possible_token_types:
             if self.__is_token(token_type):
-                operator = self.__current_token_value()
-                self.__next_token()
+                operator = self.__current_token_value_then_next()
                 if (right_expression := self.__try_parse_additive_expression()) is None:
                     raise UnexpectedTokenException(self.__current_token())
                 return RelationCondition(negated, left_expression, operator, right_expression)
@@ -265,36 +232,29 @@ class SyntacticAnalyzer:
 
     def __try_parse_literal(self):
         if self.__is_token(TokenType.STRING):
-            literal = StringLiteral(self.__current_token_value())
-            self.__next_token()
+            literal = StringLiteral(self.__current_token_value_then_next())
         elif self.__is_token(TokenType.NUMBER):
-            literal = NumberLiteral(self.__current_token_value())
-            self.__next_token()
+            literal = NumberLiteral(self.__current_token_value_then_next())
         else:
             literal = self.__try_parse_matrix_literal()
 
         return literal
 
     def __try_parse_matrix_literal(self):
-        if not self.__is_token(TokenType.OPEN_SQUARE_BRACKET):
+        if not self.__is_token_then_next(TokenType.OPEN_SQUARE_BRACKET):
             return None
-        # Consume opening '[' bracket.
-        self.__next_token()
         if (expression := self.__try_parse_additive_expression()) is None:
             raise UnexpectedTokenException(self.__current_token())
         expressions = [expression]
         separators = []
         while self.__is_token(TokenType.COMMA) or self.__is_token(TokenType.SEMICOLON):
-            separators.append(self.__current_token_value())
-            self.__next_token()
+            separators.append(self.__current_token_value_then_next())
             if (expression := self.__try_parse_additive_expression()) is None:
                 # After the separator, there must be following expression.
                 raise UnexpectedTokenException(self.__current_token())
             expressions.append(expression)
-        if not self.__is_token(TokenType.CLOSE_SQUARE_BRACKET):
+        if not self.__is_token_then_next(TokenType.CLOSE_SQUARE_BRACKET):
             raise MissingBracketException(TokenType.CLOSE_SQUARE_BRACKET, self.__current_token())
-        # Consume ']' in order to maintain invariant.
-        self.__next_token()
 
         return MatrixLiteral(expressions, separators)
 
@@ -304,12 +264,16 @@ class SyntacticAnalyzer:
     def __next_token(self):
         self.token = self.lexer.next_token()
 
-    def __is_next_token(self, expected_token_type):
-        self.__next_token()
-        return self.__is_token(expected_token_type)
-
     def __is_token(self, expected_token_type):
         return self.token.type == expected_token_type
 
-    def __current_token_value(self):
-        return self.token.value
+    def __is_token_then_next(self, expected_token_type):
+        if self.token.type == expected_token_type:
+            self.__next_token()
+            return True
+        return False
+
+    def __current_token_value_then_next(self):
+        value = self.token.value
+        self.__next_token()
+        return value
